c03d525765bb37597e743ff26dbd4092094a48bb
pavelVPo <97514993+pavelVPo@users.noreply.github.com>
2026-03-03 15:41:54 +0300

    Add files via upload

--- table.js
+++ table.js
@@ -1,7 +1,6 @@
 //Function to populate the table, SEE: https://developer.mozilla.org/en-US/docs/Web/API/HTMLTableElement/insertRow
 //Table here is the table's body
 const update_table = async function (table, values, names_arr) {
-	console.log("Table");
 	//Clear table
 	table.innerHTML = "";
 	//Populate table
@@ -13,7 +12,7 @@ const update_table = async function (table, values, names_arr) {
 			let new_cell = new_row.insertCell(i);
 			//Create text
 			if(i === 4) {
-				new_cell.innerHTML = '<a class = "link_target_chembl" href="https://www.ebi.ac.uk/chembl/explore/target/' + value[names_arr[i]] + '" target = "_blank">' + value[names_arr[i]] + '</a>';
+				new_cell.innerHTML = '<a class = "link_target_chembl" href="https://www.ebi.ac.uk/chembl/explore/target/' + value[names_arr[i]].replace(/_.*/, "") + '" target = "_blank">' + value[names_arr[i]] + '</a>';
 			} else {
 				new_cell.innerHTML = value[names_arr[i]];
 			}
